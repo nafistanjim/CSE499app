f7801154c647fe4731e8f2efec99a6e9df09d274
Azfar Shams <azfarshams1998@gmail.com>
2022-12-19 14:32:39 +0600

    Update pos.php

--- pages/pos.php
+++ pages/pos.php
@@ -118,7 +118,7 @@ function pre_r($array){
                             <form action="" method="GET" name="">
 	                            <table>
 		                            <tr>
-			                            <td><input type="text" name="k" value="<?php echo isset($_GET['k']) ? $_GET['k'] : ''; ?>" placeholder="Enter your search keywords" /></td>
+			                            <td><input type="text" name="k" value="<?php echo isset($_GET['k']) ? $_GET['k'] : ''; ?>" placeholder="Search" /></td>
 			                            <td><input type="submit" name="" value="Search" /></td>
 		                            </tr>
 	                            </table>
